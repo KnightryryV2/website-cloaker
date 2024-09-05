180e2967cbd29e1ccc2d5649f751e76ab21203f8
KnightryryV2 <116779059+KnightryryV2@users.noreply.github.com>
2024-09-05 12:05:17 -0400

    ig try this

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// totally didnt copy this shit from this stack overflow url 
-// https://stackoverflow.com/questions/77512444/how-can-i-make-a-popup-about-blank-window-with-an-embedded-page-in-it
-function cloak() {
-    var url = prompt("Please enter the URL.")
-    var fullscreenBtn = document.getElementById("fullscreen-btn");
-    fullscreenBtn.addEventListener("click", function() { var elem = document.documentElement; 
-    if (elem.requestFullscreen) { elem.requestFullscreen(); } else if (elem.webkitRequestFullscreen) { /* Safari */ elem.webkitRequestFullscreen(); } else if (elem.msRequestFullscreen) { /* IE11 */ elem.msRequestFullscreen(); } window.open(url, "_blank"); });
-}
+// totally didnt copy this shit from this from a google dev forum
+var url = "https://sites.google.com/view/un35/home"; 
+var fullscreenBtn = document.getElementById("fullscreen-btn");
+fullscreenBtn.addEventListener("click", function() { var elem = document.documentElement; 
+if (elem.requestFullscreen) { elem.requestFullscreen(); } else if (elem.webkitRequestFullscreen) { /* Safari */ elem.webkitRequestFullscreen(); } else if (elem.msRequestFullscreen) { /* IE11 */ elem.msRequestFullscreen(); } window.open(url, "_blank"); });
